b73308433fe287bf05e74c70eaced0b92468fc5c
Akilan <akil.dove@gmail.com>
2018-01-26 11:13:24 +0530

    mysql build also added

--- docker-compose-build/sql/initial.sql
+++ docker-compose-build/sql/initial.sql
@@ -2,4 +2,4 @@ CREATE DATABASE if not exists employee;
 
 USE employee;
 
-CREATE TABLE if not exists employee ( id int not null auto_increment, name varchar(255),role varchar(255), primary key (id));
+CREATE TABLE if not exists s ( id int not null auto_increment, name varchar(255),role varchar(255), primary key (id));
